c0caa760b155eff236af2634e940171ac96d1938
hanaheyo <violets5618@gmail.com>
2020-11-23 10:40:00 +0900

    Angular Router - 01. using router

--- angular-router-example/src/app/app.module.ts
+++ angular-router-example/src/app/app.module.ts
@@ -1,15 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Route, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-
-const routes: Route[] = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent }
-];
 
 @NgModule({
   declarations: [
@@ -19,7 +14,7 @@ const routes: Route[] = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
